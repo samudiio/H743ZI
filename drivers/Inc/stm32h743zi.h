ce3dc0cfb3a098b16883e62a2c0b945c9f87a2b9
Samuel <samymtz66@gmail.com>
2021-01-25 15:15:01 -0600

    Add AHBx and APBx Bus periphearal base addresses.

--- drivers/Inc/stm32h743zi.h
+++ drivers/Inc/stm32h743zi.h
@@ -34,4 +34,19 @@
 /* System memory*/
 #define ROM_BASEADDR            0x1FF00000U
 
+/**********************************************
+ * AHBx and APBx Bus Peripheral base addresses
+ ***********************************************/
+
+#define PERIPH_BASE             0x40000000U
+
+#define APB1PERIPH_BASE         PERIPH_BASE
+#define APB2PERIPH_BASE         0x40010000U
+#define AHB1PERIPH_BASE         0x40020000U
+#define AHB2PERIPH_BASE         0x48020000U
+#define APB3PERIPH_BASE         0x48023000U
+#define AHB3PERIPH_BASE         0x51000000U
+#define APB4PERIPH_BASE         0x58000000U
+#define AHB4PERIPH_BASE         0x58020000U
+
 #endif /* INC_STM32H743ZI_H_ */
