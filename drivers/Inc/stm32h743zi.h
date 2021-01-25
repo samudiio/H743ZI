8148f4e1a81001f57027993f952e81c0951c667f
Samuel <samymtz66@gmail.com>
2021-01-25 15:10:07 -0600

    Add memory base addresses section.

--- drivers/Inc/stm32h743zi.h
+++ drivers/Inc/stm32h743zi.h
@@ -10,6 +10,28 @@
 #define INC_STM32H743ZI_H_
 
 
+/**********************************************
+ * Base addresses of Flash and SRAM memories
+ ***********************************************/
+#define FLASH_BASEADDR          0x08000000U
 
+/* D1 domain, AXI SRAM */
+#define SRAM_BASEADDR           0x24000000U
+
+/* D2 domain, AHB SRAM */
+#define SRAM1_BASEADDR          0x30000000U
+#define SRAM2_BASEADDR          0x30020000U
+#define SRAM3_BASEADDR          0x30040000U
+
+/* D3 domain, AHB SRAM */
+#define SRAM4_BASEADDR          0x30080000U
+#define BACKUP_SRAM_BASEADDR    0x38800000U
+
+/* The TCM SRAMs are dedicated to the Cortex®-M7 */
+#define DTCM_RAM_BASEADDR       0x20000000U
+#define ITCM_RAM_BASEADDR       0x00000000U
+
+/* System memory*/
+#define ROM_BASEADDR            0x1FF00000U
 
 #endif /* INC_STM32H743ZI_H_ */
