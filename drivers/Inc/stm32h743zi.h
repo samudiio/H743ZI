c1415d1ae1f85c3e861756e296836e142542e707
Samuel <samymtz66@gmail.com>
2021-01-25 15:06:52 -0600

    Create device header file

---
+++ drivers/Inc/stm32h743zi.h
@@ -0,0 +1,15 @@
+/*
+ * stm32h743zi.h
+ *
+ *  Created on: Jan 19, 2021
+ *      Author: Samuel
+ *      This header file describes the MCU STM32H743ZI
+ */
+
+#ifndef INC_STM32H743ZI_H_
+#define INC_STM32H743ZI_H_
+
+
+
+
+#endif /* INC_STM32H743ZI_H_ */
