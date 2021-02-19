b58ed123f7084e64be3efb5e7393534b754ca68b
Samuel <samymtz66@gmail.com>
2021-02-18 20:12:33 -0600

    Add GPIO interrupts examples.

--- src/gpio/gpio_examples.c
+++ src/gpio/gpio_examples.c
@@ -5,6 +5,7 @@
  *      Author: Samuel
  */
 
+#include <string.h>
 #include "gpio_examples.h"
 #include "stm32h743zi.h"
 
@@ -70,7 +71,7 @@ void LED_Toggling_With_OpenDrain_Cfg(void)
     }
 }
 
-void Led_Button(void)
+void IntButton_Led(void)
 {
     GPIO_Handle_t GpioLed;
     GPIO_Handle_t GpioBtn;
@@ -108,7 +109,7 @@ void Led_Button(void)
     }
 }
 
-void Led_Button_Ext(void)
+void ExtButton_Led(void)
 {
     GPIO_Handle_t GpioLed;
     GPIO_Handle_t GpioBtn;
@@ -146,3 +147,95 @@ void Led_Button_Ext(void)
     }
 }
 
+void ExtButton_Led_Interrupt(void)
+{
+    GPIO_Handle_t GpioLed;
+    GPIO_Handle_t GpioBtn;
+
+    memset(&GpioLed, 0x00, sizeof(GpioLed));
+    memset(&GpioBtn, 0x00, sizeof(GpioBtn));
+
+    /* LED GPIO configuration */
+    GpioLed.pGPIOx = GPIOB;
+    GpioLed.GPIO_PinConfig.GPIO_PinNumber = LED2_BLUE;
+    GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    GPIO_PeriClockControl(GPIOB, ENABLE);
+
+    GPIO_Init(&GpioLed);
+
+    /* Button GPIO configuration */
+    GpioBtn.pGPIOx = GPIOD;
+    GpioBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+    GpioBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_FT;
+    GpioBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+
+    GPIO_PeriClockControl(GPIOD, ENABLE);
+
+    GPIO_Init(&GpioBtn);
+
+    GPIO_WriteToOutputPin(GPIOB, LED1_GREEN, GPIO_PIN_RESET);
+    /* IRQ Configurations */
+    GPIO_IRQPriorityConfig(IRQ_NO_EXTI9_5, NVIC_IRQ_PRIO15);
+    GPIO_IRQConfig(IRQ_NO_EXTI9_5, ENABLE);
+
+    while(1);
+
+}
+
+void IntButton_Led_Interrupt(void)
+{
+    GPIO_Handle_t GpioLed;
+    GPIO_Handle_t GpioBtn;
+
+    memset(&GpioLed, 0x00, sizeof(GpioLed));
+    memset(&GpioBtn, 0x00, sizeof(GpioBtn));
+
+    /* LED GPIO configuration */
+    GpioLed.pGPIOx = GPIOB;
+    GpioLed.GPIO_PinConfig.GPIO_PinNumber = LED3_RED;
+    GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+    GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    GPIO_PeriClockControl(GPIOB, ENABLE);
+
+    GPIO_Init(&GpioLed);
+
+    /* Button GPIO configuration */
+    GpioBtn.pGPIOx = GPIOC;
+    GpioBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+    GpioBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_RT;
+    GpioBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+    GpioBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    GPIO_PeriClockControl(GPIOC, ENABLE);
+
+    GPIO_Init(&GpioBtn);
+
+    /* IRQ Configurations */
+    GPIO_IRQPriorityConfig(IRQ_NO_EXTI15_10, NVIC_IRQ_PRIO2);
+    GPIO_IRQConfig(IRQ_NO_EXTI15_10, ENABLE);
+
+    while(1);
+}
+
+void EXTI9_5_IRQHandler(void)
+{
+    /* Handle the interrupt */
+    delay(); /* wait till button de-bouncing gets over */
+    GPIO_IRQHandling(GPIO_PIN_NO_5);
+    GPIO_ToggleOutputPin(GPIOB, LED2_BLUE);
+}
+
+void EXTI15_10_IRQHandler(void)
+{
+    delay(); /* wait till button de-bouncing gets over */
+    GPIO_IRQHandling(GPIO_PIN_NO_13);
+    GPIO_ToggleOutputPin(GPIOB, LED3_RED);
+}
--- src/gpio/gpio_examples.h
+++ src/gpio/gpio_examples.h
@@ -10,7 +10,9 @@
 
 void LED_Toggling_With_PushPull_Cfg(void);
 void LED_Toggling_With_OpenDrain_Cfg(void);
-void Led_Button(void);
-void Led_Button_Ext(void);
+void IntButton_Led(void);
+void ExtButton_Led(void);
+void ExtButton_Led_Interrupt(void);
+void IntButton_Led_Interrupt(void);
 
 #endif /* GPIO_EXAMPLES_H_ */
