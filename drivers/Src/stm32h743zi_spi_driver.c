af647a32144f2924d86a5d06338e7bb90123dba1
Samuel <samymtz66@gmail.com>
2021-04-01 23:34:07 -0600

    Implementation of SPI perioheral clock control API.

--- drivers/Src/stm32h743zi_spi_driver.c
+++ drivers/Src/stm32h743zi_spi_driver.c
@@ -17,7 +17,50 @@
  */
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 {
-
+    if(EnorDi == ENABLE)
+    {
+        if(pSPIx == SPI1)
+        {
+            SPI1_PCLK_EN();
+        }else if (pSPIx == SPI2)
+        {
+            SPI2_PCLK_EN();
+        }else if (pSPIx == SPI3)
+        {
+            SPI3_PCLK_EN();
+        }else if (pSPIx == SPI4)
+        {
+            SPI4_PCLK_EN();
+        }else if (pSPIx == SPI5)
+        {
+            SPI5_PCLK_EN();
+        }else if (pSPIx == SPI6)
+        {
+            SPI6_PCLK_EN();
+        }
+    }
+    else
+    {
+        if(pSPIx == SPI1)
+        {
+            SPI1_PCLK_DI();
+        }else if (pSPIx == SPI2)
+        {
+            SPI2_PCLK_DI();
+        }else if (pSPIx == SPI3)
+        {
+            SPI3_PCLK_DI();
+        }else if (pSPIx == SPI4)
+        {
+            SPI4_PCLK_DI();
+        }else if (pSPIx == SPI5)
+        {
+            SPI5_PCLK_DI();
+        }else if (pSPIx == SPI6)
+        {
+            SPI6_PCLK_DI();
+        }
+    }
 }
 
 /*********************************************************************
